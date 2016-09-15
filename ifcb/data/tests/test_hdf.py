efa17f961e93683f746212f3f5768f85a66747a6
Joe Futrelle <joefutrelle@gmail.com>
2016-09-15 21:22:59 +0000

    fixed failing test to use proper HDF opening semantics

--- ifcb/data/tests/test_hdf.py
+++ ifcb/data/tests/test_hdf.py
@@ -115,12 +115,12 @@ class TestHdfBin(unittest.TestCase):
                 out_bin.to_hdf(h, out_bin.lid)
                 with HdfBin(h, out_bin.lid) as in_bin:
                     assert_bin_equals(in_bin, out_bin)
-    @unittest.skip('failing')
     @withfile
     def test_multiple_closed_group(self, path):
-        for fs in list_test_filesets():
-            out_bin = FilesetBin(fs)
-            out_bin.to_hdf(path, out_bin.lid)
+        with hdfopen(path, replace=True) as h:
+            for fs in list_test_filesets():
+                out_bin = FilesetBin(fs)
+                out_bin.to_hdf(h, out_bin.lid)
         for fs in list_test_filesets():
             out_bin = FilesetBin(fs)
             with HdfBin(path, out_bin.lid) as in_bin:
