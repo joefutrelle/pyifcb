aed7e512a7a31033a4b004d4377930addd32858a
Joe Futrelle <joefutrelle@gmail.com>
2016-09-21 18:03:23 +0000

    fixed typo

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ release = '0.1'
 #language = None
 
 # There are two options for replacing |today|: either, you set today to some
-[# non-false value, then it is used:
+# non-false value, then it is used:
 #today = ''
 # Else, today_fmt is used as the format for a strftime call.
 #today_fmt = '%B %d, %Y'
