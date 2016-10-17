9091071e612c2a8ce9bde7c6bcf95fd2b23423b8
Joe Futrelle <joefutrelle@gmail.com>
2016-10-17 18:33:41 +0000

    added test for BaseDictlike

---
+++ ifcb/data/tests/test_base_dictlike.py
@@ -0,0 +1,43 @@
+import unittest
+
+from ..utils import BaseDictlike
+
+class MinimalBD(BaseDictlike):
+    def __init__(self, a_dict):
+        self.dict = a_dict
+    def iterkeys(self):
+        return self.dict.iterkeys()
+    def __getitem__(self, k):
+        return self.dict[k]
+            
+class TestBaseDictlike(unittest.TestCase):
+    def setUp(self):
+        self.keys = 'abc'
+        self.values = '123'
+        self.bad_keys = 'xyz'
+        self.d = dict(zip(self.keys, self.values))
+        self.bd = MinimalBD(self.d)
+    def test_not_implemented(self):
+        bd = BaseDictlike()
+        with self.assertRaises(NotImplementedError):
+            bd.iterkeys()
+        with self.assertRaises(NotImplementedError):
+            bd[0]
+    def test_iter(self):
+        bd_keys = set(self.bd)
+        assert set(self.keys) == bd_keys
+    def test_keys(self):
+        assert set(self.keys) == set(self.bd.keys())
+    def test_values(self):
+        assert set(self.values) == set(self.bd.values())
+    def test_contains(self):
+        for k in self.keys:
+            assert self.bd.has_key(k)
+            assert k in self.bd
+        for k in self.bad_keys:
+            assert not self.bd.has_key(k)
+            assert k not in self.bd
+    def test_len(self):
+        assert len(self.keys) == len(self.bd)
+
+        
