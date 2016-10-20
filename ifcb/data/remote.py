35a919b9d75d84d2fcab3c6c31d96830f8f80132
Joe Futrelle <joefutrelle@gmail.com>
2016-10-20 14:17:16 +0000

    added docstring

--- ifcb/data/remote.py
+++ ifcb/data/remote.py
@@ -10,6 +10,20 @@ from .files import Fileset, FilesetBin
 
 @contextmanager
 def remote_bin(base_url):
+    """
+    Context manager for remote access to a bin. Stages
+    files to a temporary directory and creates a ``FilesetBin``
+    backed by them.
+
+    :param url: the base URL of the remote files
+
+    :Example:
+
+    >>> with remote_bin('http://mysite.org/ifcb/D20170801T023142_IFCB113') as b:
+    ...     im = b.images[32]
+
+
+    """
     base_url = os.path.splitext(base_url)[0]
     d = tempfile.mkdtemp()
     base_path = os.path.join(d, Pid(base_url).bin_lid)
