6875fa9eab8e2f876c36d619c83e855d3b248b41
Joe Futrelle <joefutrelle@gmail.com>
2016-10-31 18:51:37 +0000

    header roundtripping

--- ifcb/data/matlab.py
+++ ifcb/data/matlab.py
@@ -11,6 +11,20 @@ PID_VAR = 'pid'
 ADC_VAR = 'adc'
 ROI_NUMBERS_VAR = 'roi_numbers'
 IMAGES_VAR = 'images'
+HEADER_NAMES_VAR = 'header_names'
+HEADERS_VAR = 'headers'
+
+MAX_FIELD_LENGTH = 31
+
+# notes about MATLAB format
+# the matrix of ADC data is of a uniform type, so int columns
+# come back as floats.
+# the max field name length for structs is 31 characters (at
+# least in scipy.io), so field names in the struct are truncated
+# to that max length.
+# a separate list of field names is stored in the .mat file.
+# that is right-space-padded upon reading, so the padding is
+# stripped.
 
 def bin2mat(b, mat_path):
     # ADC data
@@ -18,8 +32,11 @@ def bin2mat(b, mat_path):
     # warning: reads all images into memory
     roi_numbers = sorted(b.images)
     images = [b.images[r] for r in roi_numbers]
+    headers = { k[:MAX_FIELD_LENGTH]: v for k,v in b.headers.items() }
     savemat(mat_path, {
         PID_VAR: str(b.lid), # remove non-bin parts of pid
+        HEADER_NAMES_VAR: sorted(b.headers.keys()),
+        HEADERS_VAR: headers,
         ADC_VAR: adc,
         ROI_NUMBERS_VAR: roi_numbers,
         IMAGES_VAR: images
@@ -50,3 +67,6 @@ class MatBin(BaseBin):
         self.adc = pd.DataFrame(self._mat[ADC_VAR]);
         self.adc.index += 1 # 1-based indexes
         self.images = _MatBinImages(self._mat)
+        _headers = self._mat[HEADERS_VAR]
+        _names = [ n.rstrip(' ') for n in self._mat[HEADER_NAMES_VAR] ]
+        self.headers = { n: _headers[n[:MAX_FIELD_LENGTH]] for n in _names }
